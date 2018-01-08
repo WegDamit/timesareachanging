a8835d11dfaa0c17e4f720dd6705fb9f4c710825
Andreas Rabus <Andreas.Rabus@gmail.com>
2018-01-08 14:39:39 +0100

    using REST

--- src/api.js
+++ src/api.js
@@ -1,17 +1,27 @@
 import feathers from '@feathersjs/feathers'
-// BUG: https://github.com/feathersjs/feathers/issues/756
-import socketio from '@feathersjs/socketio-client'
 import auth from '@feathersjs/authentication-client'
-import io from 'socket.io-client'
+// BUG: https://github.com/feathersjs/feathers/issues/756
+// import socketio from '@feathersjs/socketio-client'
+// import io from 'socket.io-client'
+// const socket = io('http://localhost:3030', {transports: ['websocket']})
 
-const socket = io('http://localhost:3030', {transports: ['websocket']})
+// Connect to the same as the browser URL (only in the browser)
+const rest = require('@feathersjs/rest-client')
+// const restClient = rest()
+const restClient = rest('http://localhost:3030')
+const localstorage = require('feathers-localstorage')
+const storage = require('localstorage-memory')
 
 const api = feathers()
   // .configure(hooks())
-  .configure(socketio(socket))
-  .configure(auth({ storage: window.localStorage }))
+  // .configure(socketio(socket))
+  // .configure(auth({ storage: window.localStorage }))
+  .use('storage', localstorage({storage: storage}))
+  .configure(restClient.fetch(window.fetch))
+  .configure(auth())
 
 api.service('/users')
 api.service('/workhours')
+api.service('/projects')
 
 export default api
